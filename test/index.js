25797b08eb3dfd61c01138f9a16199068489eb41
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-04-16 12:08:25 +0200

    Refactor tests

--- test/index.js
+++ test/index.js
@@ -1,10 +1,10 @@
 'use strict'
 
-var parseJson = require('parse-json')
 var should = require('should')
 var path = require('path')
-var jsonFuture = require('..')
 var fs = require('fs')
+
+var jsonFuture = require('..')
 
 var FIXTURES = {
   string: '{\n  "foo": "bar"\n}\n',
@@ -23,23 +23,23 @@ describe('JSON Future ::', function () {
   describe('sync', function () {
     it('.parse', function () {
       var json = jsonFuture.parse(FIXTURES.string)
-      json.foo.should.be.equal(FIXTURES.object.foo)
+      should(json.foo).be.equal(FIXTURES.object.foo)
     })
 
     it('.stringify', function () {
       var string = jsonFuture.stringify(FIXTURES.object)
-      string.should.be.equal(FIXTURES.string)
+      should(string).be.equal(FIXTURES.string)
     })
 
     it('.load', function () {
       var json = jsonFuture.load(FIXTURES.path)
-      json.foo.should.be.equal(FIXTURES.object.foo)
+      should(json.foo).be.equal(FIXTURES.object.foo)
     })
 
     it('.save', function () {
       jsonFuture.save(FIXTURES.path2, {hello: 'world'})
       var json = jsonFuture.load(FIXTURES.path2)
-      json.hello.should.be.equal('world')
+      should(json.hello).be.equal('world')
     })
   })
 
@@ -47,7 +47,7 @@ describe('JSON Future ::', function () {
     describe('.parseAsync', function () {
       it('String', function (done) {
         jsonFuture.parseAsync(FIXTURES.string, function (err, data) {
-          data.foo.should.be.equal(FIXTURES.object.foo)
+          should(data.foo).be.equal(FIXTURES.object.foo)
           done(err)
         })
       })
@@ -55,7 +55,7 @@ describe('JSON Future ::', function () {
       it('Buffer', function (done) {
         var buff = new Buffer(FIXTURES.string)
         jsonFuture.parseAsync(buff, function (err, data) {
-          data.foo.should.be.equal(FIXTURES.object.foo)
+          should(data.foo).be.equal(FIXTURES.object.foo)
           done(err)
         })
       })
@@ -63,14 +63,14 @@ describe('JSON Future ::', function () {
 
     it('.stringifyAsync', function (done) {
       jsonFuture.stringifyAsync(FIXTURES.object, function (err, string) {
-        string.should.be.equal(FIXTURES.string)
+        should(string).be.equal(FIXTURES.string)
         done(err)
       })
     })
 
     it('.loadAsync', function (done) {
       jsonFuture.loadAsync(FIXTURES.path, function (err, json) {
-        json.foo.should.be.equal(FIXTURES.object.foo)
+        should(json.foo).be.equal(FIXTURES.object.foo)
         done(err)
       })
     })
@@ -81,7 +81,7 @@ describe('JSON Future ::', function () {
       }, function (err) {
         if (err) return done(err)
         jsonFuture.loadAsync(FIXTURES.path2, function (err, json) {
-          json.hello.should.be.equal('world2')
+          should(json.hello).be.equal('world2')
           done(err)
         })
       })
